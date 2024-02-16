4f990434cc5a28d45f34b760726b1600c335f7b8
deep9685 <deepaknmdv14@gmail.com>
2024-02-16 13:09:25 +0530

    Logic behind functionality has been written

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
 
   const [courses, setCourses] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
 
   async function fetchData() {
     setLoading(true);
@@ -36,21 +37,26 @@ const App = () => {
   }, []) 
 
     return (
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen flex flex-col bg-bgDark2">
           <div>
-              <Navbar/>
+          <Navbar />
+        </div>
+
+        <div className="bg-bgDark2">
+          <div>
+            <Filter filterData={filterData} 
+              category={category}
+              setCategory = {setCategory}
+            />
           </div>
 
-          <div>
-              <Filter filterData = {filterData} />
+          <div
+            className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap
+                      justify-center items-center min-h-[50vh]"
+          >
+            {loading ? <Spinner /> : <Cards courses={courses} category={category} />}
           </div>
-
-          <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap
-                      justify-center items-center min-h-[50vh]">
-              {
-                loading ? (<Spinner/>) : (<Cards courses = {courses}/>)
-              }
-          </div>
+        </div>
       </div>
     );
 };
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,5 @@
 // import { getAllByDisplayValue } from '@testing-library/react'
-import React from 'react';
+import React, { useState } from 'react';
 import Card from "./Card";
 
 const Cards = (props) => {
@@ -7,24 +7,35 @@ const Cards = (props) => {
     // console.log(props.courses);
 
     let courses = props.courses;
+    let category = props.category;
+
+    const [likedCourses, setLikedCourses] = useState([]);
 
     // returns you a list of all courses received from api response
     function getCourses(){
-        let allCourses = [];
-        Object.values(courses).forEach(array => {
-            array.forEach(courseData => {
+        if(category === "All")
+        {
+            let allCourses = [];
+            Object.values(courses).forEach((array) => {
+              array.forEach((courseData) => {
                 allCourses.push(courseData);
-            })
-        })
-        return allCourses;
+              });
+            });
+            return allCourses;
+        }
+        else{
+            // main sirf specific category ka data array pass karunga
+            return courses[category];
+        }
+        
     }
 
   return (
     
-    <div>
+    <div className='flex flex-wrap justify-center gap-4 mb-4'>
         {
             getCourses().map((course) => (
-                <Card key = {course.id} course = {course}/>
+                <Card key = {course.id} course = {course} likedCourses={likedCourses} setLikedCourses={setLikedCourses}/>
             ))
         }
     </div>
